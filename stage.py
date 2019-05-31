d8e3ee2701e8b42f8c72c0e08c96c55c8a4b801f
shalz6613 <shalz6613@wrdsb.ca>
2019-05-31 14:11:51 -0400

    prepering for collisions

--- stage.py
+++ stage.py
@@ -9,9 +9,10 @@ groundBounds = list()
 def createStage():
     global platforms
     platforms = pygame.Surface(gameVariables.screenSize, pygame.SRCALPHA)
-    groundBounds.append(gameFunctions.drawRectangle((0, 100), (gameVariables.screenSize[0], 0)))
+    groundBounds.append(gameFunctions.drawRectangle(placeAt((0, 10)), placeAt((100, 0))))
     groundBounds.append(gameFunctions.drawRectangle(placeAt((20, 30)), placeAt((40, 35))))
     groundBounds.append(gameFunctions.drawRectangle(placeAt((50, 30)), placeAt((60, 35))))
+    groundBounds.append(gameFunctions.drawRectangle(placeAt((45, 45)), placeAt((60, 50))))
     for i in groundBounds:
         gameFunctions.fillArea(platforms, gameFunctions.loadImage("briks.jpg", 1.2), i)
 
