f57de2db534fa70f711891c8807ae48b350cec73
gibbw6465 <gibbw6465@wrdsb.ca>
2019-06-04 12:24:02 -0400

    Updated text to beter fit Bullet Rain

--- gameVariables.py
+++ gameVariables.py
@@ -10,8 +10,8 @@ keys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o
         "w", "x", "y", "z", "-", "_"]
 
 actions = "jump sneak right left special1 special2".split()
-player1_keys = [pygame.K_w, pygame.K_s, pygame.K_d, pygame.K_a, pygame.K_q, pygame.K_e]
-player2_keys = [pygame.K_KP8, pygame.K_KP5, pygame.K_KP6, pygame.K_KP4, pygame.K_KP7, pygame.K_KP9]
+player1_keys = [pygame.K_w, pygame.K_s, pygame.K_d, pygame.K_a, pygame.K_n, pygame.K_m]
+player2_keys = [pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_SLASH, pygame.K_RSHIFT]
 player1_controls = dict(zip(actions, player1_keys))
 player2_controls = dict(zip(actions, player2_keys))
 
