c026895dcd62ff9a8b05a33bfeaf614a55887d95
shalz6613 <shalz6613@wrdsb.ca>
2019-06-05 14:05:27 -0400

    multiple stages

--- stage.py
+++ stage.py
@@ -3,8 +3,16 @@ import gameFunctions
 import gameVariables
 from gameFunctions import placeAt
 
+stage1 = [gameFunctions.drawRectangle(placeAt((20, 30)), placeAt((40, 35))),
+          gameFunctions.drawRectangle(placeAt((60, 30)), placeAt((80, 35))),
+          gameFunctions.drawRectangle(placeAt((45, 59)), placeAt((65, 56))),
+          gameFunctions.drawRectangle(placeAt((30, 60)), placeAt((32, 100))),
+          gameFunctions.drawRectangle(placeAt((20, 75)), placeAt((35, 70)))]
 
-def createStage():
+stage2 = []
+
+
+def createStage(stage):
     global platforms
     platforms = pygame.Surface(gameVariables.screenSize, pygame.SRCALPHA)
 
@@ -13,17 +21,11 @@ def createStage():
               gameFunctions.drawRectangle(placeAt((105, 0)), placeAt((100, 100))),
               gameFunctions.drawRectangle(placeAt((0, 105)), placeAt((100, 100)))]
 
-    landings = [gameFunctions.drawRectangle(placeAt((20, 30)), placeAt((40, 35))),
-                gameFunctions.drawRectangle(placeAt((60, 30)), placeAt((80, 35))),
-                gameFunctions.drawRectangle(placeAt((45, 59)), placeAt((65, 56))),
-                gameFunctions.drawRectangle(placeAt((30, 60)), placeAt((32, 100))),
-                gameFunctions.drawRectangle(placeAt((20, 75)), placeAt((35, 70)))]
-
     for i in floors:
         gameVariables.obstecls.append(i)
         gameFunctions.fillArea(platforms, gameFunctions.loadImage("ground.jpg", .4), i)
 
-    for i in landings:
+    for i in stage:
         gameVariables.obstecls.append(i)
         gameFunctions.fillArea(platforms, gameFunctions.loadImage("briks.jpg", 1.2), i)
 
